7a94a853d6900d72e26b9f86b8816e372ca2eaa4
Outsrkem <xhdascnf@126.com>
2020-05-02 18:53:31 +0800

    update redis/2.8.6/docker-entrypoint.sh.

--- redis/2.8.6/docker-entrypoint.sh
+++ redis/2.8.6/docker-entrypoint.sh
@@ -2,14 +2,15 @@
 set -e
 
 if [ "$1" == 'redis-server' ]; then
-	echo "------>  --env REDIS_PORT=3345   端口修改"
+    echo "------>  --env REDIS_PORT=3345   端口修改"
+    echo
 
-	if [ ! -z "$REDIS_PORT" ];then
-		sed -i "s/port 6379/port ${REDIS_PORT}/g"  /usr/local/redis/etc/redis.conf
-		echo
-		echo "------>  redis 容器内新端口为 $REDIS_PORT "
-		echo
-	fi
+    if [ ! -z "$REDIS_PORT" ];then
+        sed -i "s/port 6379/port ${REDIS_PORT}/g"  /usr/local/redis/etc/redis.conf
+        echo
+        echo "------>  redis 容器内新端口为 $REDIS_PORT "
+        echo
+    fi
 fi
 
 exec "$@"
