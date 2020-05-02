107ae102e887b1f63d38da0b5b4b2305d1e0b77f
Outsrkem <xhdascnf@126.com>
2020-05-02 17:53:44 +0800

    2020年05月 2日 17:53:43 更新文件

--- redis/2.8.6/docker-entrypoint.sh
+++ redis/2.8.6/docker-entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -e
 
-if [ "$1" = 'redis-server' ]; then
+if [ "$1" == 'redis-server' ]; then
 	echo "------>  --env REDIS_PORT=3345   端口修改"
 
-	if [ ! -z "$MYSQL_PORT" ];then
-		sed -i "s/port 6379/port ${MYSQL_PORT}/g"  /usr/local/redis/etc/redis.conf
+	if [ ! -z "$REDIS_PORT" ];then
+		sed -i "s/port 6379/port ${REDIS_PORT}/g"  /usr/local/redis/etc/redis.conf
 		echo
-		echo "------>  redis 容器内新端口为 $MYSQL_PORT "
+		echo "------>  redis 容器内新端口为 $REDIS_PORT "
 		echo
 	fi
 fi
 
-exec $@
+exec "$@"
