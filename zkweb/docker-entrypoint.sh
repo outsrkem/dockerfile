65ce81238e89c77e09b0bd50eff101f724bcf19a
Outsrkem <xhdascnf@126.com>
2020-12-02 22:17:28 +0800

    akweb

---
+++ zkweb/docker-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+if [ "${1:0:4}" == '-jar' ]; then
+    JAVA_OPTS='-Xmx4096m -Xms4096m -server -Dfile.encoding=UTF-8 -Duser.language=zh -Duser.region=CN -Djava.security.egd=file:/dev/./urandom'
+    set -- java ${JAVA_OPTS} "$@"
+fi
+
+# 只生效与当前进程环境
+umask 0077
+
+exec "$@"
