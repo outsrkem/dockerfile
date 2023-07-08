285023ac69849f8f39c6c3fca69e31e84390b08f
Outsrkem <xhdascnf@126.com>
2023-07-08 13:11:17 +0800

    优化nginxt调试的工具镜像

--- nginx/tools/docker-entrypoint.sh
+++ nginx/tools/docker-entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 
-cd /usr/local/nginx/cert && openssl req -newkey rsa:2048 -nodes -sha256 -x509 -days 3650 \
+[ -d /etc/nginx/cert ] || mkdir /etc/nginx/cert
+
+cd /etc/nginx/cert && openssl req -newkey rsa:2048 -nodes -sha256 -x509 -days 3650 \
 -subj /CN=nginx.org \
 -keyout ./cert.key  -out ./cert.pem
 
-set -- /usr/local/nginx/sbin/nginx -g "daemon off;"
+set -- /usr/sbin/nginx -g "daemon off;"
 exec "$@"
