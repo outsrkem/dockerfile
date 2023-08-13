39825310caf1fbf35e00e0ae97670679c15a99a0
outsrkem <xhdascnf@126.com>
2023-08-13 21:02:18 +0800

    优化脚本

--- nginx/tools/docker-entrypoint.sh
+++ nginx/tools/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-[ -d /etc/nginx/cert ] || mkdir /etc/nginx/cert
+[ -d /etc/nginx/cert ] || mkdir -p /etc/nginx/cert
 
 openssl req -newkey rsa:2048 -nodes -sha256 -x509 -days 3650 -subj /CN=nginx.org \
 -keyout /etc/nginx/cert/cert.key  -out /etc/nginx/cert/cert.pem
